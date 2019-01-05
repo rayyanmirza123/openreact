b3c47bdeda0f9e1269ccdd2d5dc7543fa956da3c
rayyanmirza123 <rayyanmirza419@gmail.com>
2019-01-05 22:11:32 +0530

    Update config.php

--- Admin/config.php
+++ Admin/config.php
@@ -4,7 +4,9 @@
  *  Damn Straight now copying only buying.
  */
 
-define('DIR_MAIN',$_SERVER['SERVER_NAME']);
+$main = $_SERVER['SERVER_NAME'];
+
+define('DIR_MAIN',$main);
 define('DIR_ENGINE',DIR_MAIN."Engine/");
 define('DIR_ADMIN',"/Admin/");
 define('DIR_CONFIG',"/Engine/Config/");
