e6d0ab6af608ac5543868ed8d2dc0e7d43135f0b
rayyanmirza123 <rayyanmirza419@gmail.com>
2019-01-05 22:28:56 +0530

    Update config.php

--- Admin/config.php
+++ Admin/config.php
@@ -9,5 +9,5 @@ $main = (isset($_SERVER['HTTPS'])?"https://":"http://").$_SERVER['SERVER_NAME'];
 define('DIR_MAIN',$main);
 define('DIR_ENGINE',DIR_MAIN."/Engine/");
 define('DIR_ADMIN',DIR_MAIN."/Admin/");
-define('DIR_CONFIG',DIR_ENFINE."Config/");
+define('DIR_CONFIG',DIR_ENGINE."Config/");
 
